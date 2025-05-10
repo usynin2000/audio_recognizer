20ea65b1213c9856d4e2a28fdaa2b179fcfcc733
Usynin Sergei <usynin.s00@mail.ru>
2025-05-10 13:52:20 +0300

    here

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def extract_audio(video_path, audio_path):
     video.audio.write_audiofile(audio_path, codec="pcm_s16le", fps=16000)
 
 def transcribe_audio(audio_path):
-    model = whisper.load_model("base")  # Можно заменить на 'small', 'medium', 'large' для лучшего качества
+    model = whisper.load_model("base")
     result = model.transcribe(audio_path, language='ru')
     return result['text']
+
 
 def main(video_path):
     audio_path = "audio.wav"
@@ -20,5 +21,5 @@ def main(video_path):
     print(text)
 
 if __name__ == "__main__":
-    video_file = "video_5.mp4"  # Укажи свой путь к видео
+    video_file = "pbc_515447164_9lh8gtpepnyyj75_video_iiyrs6d7qd.wpm"  # Укажи свой путь к видео
     main(video_file)
